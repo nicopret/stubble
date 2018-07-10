103602be89d99b56d397730754226b47b6c1725c
Nico Pretorius <nico.pretorius@kryptotekbank.com>
2018-07-10 21:16:56 +0200

    Implemented tests

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,8 @@
-const createData = require("./createData"),
+const config = require("../config"),
+    createData = require("./createData"),
     createServer = require("./createServer"),
     createStructure = require("./createStructure"),
+    fs = require("fs"),
     parseContract = require("./parseContract");
 
 module.exports = {
@@ -8,7 +10,18 @@ module.exports = {
         console.log(parseContract.start());
         console.log(createStructure.start());
         console.log(createData.start());
-        console.log(createServer.start());
-        callback(true);
+        createServer.start({}, (err, res) => callback(err, res));
+    },
+    createServerPackageFile(json, callback) {
+        let params = Object.assign(json, {
+            main: config.restify.start_file,
+            name: "stubble-server",
+            scripts: {
+                start: "node ./" + config.restify.start_file
+            }
+        });
+        delete params.dependencies.nunjucks;
+        fs.writeFile([config.restify.output_folder, "package.json"].join("/"),
+            JSON.stringify(params, null, 2), (err) => callback(err, true));
     }
 };
--- test/index.unit.js
+++ test/index.unit.js
@@ -1,11 +1,18 @@
-const assert = require("assert"),
+const config = require("../config.json"),
+    fs = require("fs"),
     stubble = require("../bin/index");
 
-describe("index.js", () => {
-    describe("create", () => {
-        it("responds with true", () => {
-            let result = stubble.create();
-            assert.equal(result, true);
+describe("bin/index.js", () => {
+    describe("createServerPackageFile", () => {
+        it("create server json package", (done) => {
+            stubble.createServerPackageFile({
+                dependencies: {
+                    nunjucks: "0.0.0"
+                }
+            }, done);
+        });
+        it("ensure that json package file exist", (done) => {
+            fs.readFile([config.restify.output_folder, "package.json"].join("/"), done);
         });
     });
 });
