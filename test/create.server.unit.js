92d908d0889d850a74cf704bc351190c4bd74a38
Nico Pretorius <nico.pretorius@kryptotekbank.com>
2018-07-11 07:56:13 +0200

    create server boilerplate from templates

--- test/create.server.unit.js
+++ test/create.server.unit.js
@@ -5,12 +5,16 @@ const assert = require("assert"),
     fs = require("fs");
 
 describe("createServer index.js", () => {
-    describe("start", () => {
-        it("return true if file is created", (done) => {
-            createServer.start({}, done);
+    describe("render", () => {
+        it("only test creating one file", (done) => {
+            createServer.render({
+                structure: {},
+                target: [config.restify.output_folder, config.restify.start_file].join("/"),
+                template: config.restify.start_template
+            }, done);
         });
         it("check if file exists", (done) => {
             fs.readFile([config.restify.output_folder, config.restify.start_file].join("/"), done);
-        })
+        });
     });
 });
