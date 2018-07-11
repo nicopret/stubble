be0dbf9b1194715efc364913a787613813c34b7d
Nico Pretorius <nico.pretorius@kryptotekbank.com>
2018-07-11 08:48:59 +0200

    improved code quality

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ Stubble.create((err, result) => {
     if (err) {
         console.log(err);
         return;
-    } else {
-        fs.readFile("package.json", "utf8", (err, res) => {
-            err ? console.error(err) : 
-                Stubble.createServerPackageFile(JSON.parse(res), (err) =>
-                console.log(err ? err : "Done!!"));
-        });
     }
+    fs.readFile("package.json", "utf8", (err, res) => {
+        err ? console.error(err) : 
+            Stubble.createServerPackageFile(JSON.parse(res), (err) =>
+            console.log(err ? err : "Done!!"));
+    });
 });
