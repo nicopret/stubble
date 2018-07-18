5e4e33a0809879c74153f3d729eb9c5a29df6596
Nico Pretorius <nico.pretorius@kryptotekbank.com>
2018-07-18 12:14:34 +0200

    add contract parse test

--- test/parse.contract.unit.js
+++ test/parse.contract.unit.js
@@ -1,11 +1,11 @@
-const assert = require("assert"),
-    config = require("../config.json"),
+const config = require("../config.json"),
+    path = require("path"),
     parseContract = require("../bin/parseContract");
 
 describe("parseContract index.js", () => {
     describe("parse", () => {
         it("parse swagger contract from input file", (done) => {
-            parseContract.parse([config.restify.files_folder, config.restify.files[0]].join("/"), done);
+            parseContract.parse(path.resolve(config.restify.files_folder, config.restify.files[0]), done);
         });
     });
 });
